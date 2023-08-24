56def9a6ba6095dc9fc0cfaf5eb68ce99c2717da
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-08-24 18:07:43 +0300

    setup docker for postgres, init database

---
+++ sql/main.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS bank(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS user_(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    birth_date TEXT
+);
+
+CREATE TABLE IF NOT EXISTS account(
+                    id SERIAL PRIMARY KEY,
+                    money_ NUMERIC DEFAULT 0,
+                    user_id INT,
+                    bank_id INT,
+                    CONSTRAINT fk_user
+                        FOREIGN KEY(user_id) REFERENCES user_(id),
+                    CONSTRAINT fk_bank
+                        FOREIGN KEY(bank_id) REFERENCES bank(id)
+);
