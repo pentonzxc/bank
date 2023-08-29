d044285582b8661aa55ba4f26aafb30f81e87b16
Nikolai Urusov <nikolai.urusov@gmail.com>
2023-08-29 17:45:48 +0300

    add transaction table

--- sql/main.sql
+++ sql/main.sql
@@ -14,10 +14,23 @@ CREATE TABLE IF NOT EXISTS user_(
 CREATE TABLE IF NOT EXISTS account(
                     id SERIAL PRIMARY KEY,
                     balance NUMERIC DEFAULT 0,
-                    user_id INT,
-                    bank_id INT,
+                    user_id INT NOT NULL,
+                    bank_id INT NOT NULL,
                     CONSTRAINT fk_user
                         FOREIGN KEY(user_id) REFERENCES user_(id),
                     CONSTRAINT fk_bank
                         FOREIGN KEY(bank_id) REFERENCES bank(id)
+);
+
+CREATE TABLE IF NOT EXISTS transaction_(
+                   id uuid PRIMARY KEY,
+                   begin_date TIMESTAMP NOT NULL,
+                   end_date TIMESTAMP NOT NULL,
+                   origin_account_id INT NOT NULL,
+                   target_account_id INT,
+                   CONSTRAINT fk_origin_account
+                        FOREIGN KEY(origin_account_id) REFERENCES user_(id),
+                    CONSTRAINT fk_target_account
+                        FOREIGN KEY(target_account_id) REFERENCES user_(id)
+
 );
